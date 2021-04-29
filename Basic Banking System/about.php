80d4d2a9ae8dd9e4d75fcd30f876ce4cc464a956
its-kaustubh <83355355+its-kaustubh@users.noreply.github.com>
2021-04-29 18:43:29 +0530

    Add files via upload

---
+++ Basic Banking System/about.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <script src="https://kit.fontawesome.com/e80f78ff1f.js" crossorigin="anonymous"></script>
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=New+Tegomin&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="fixed-top">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <span class="text-muted"></span>
+    </div>
+  </div>
+
+  <?php
+  include 'navbar.php';
+  ?>
+</div>
+<section id="topcontainer">
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="https://miro.medium.com/max/1000/1*cKibMEtA00ZlfpT1bOZYkw.jpeg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://www.paisabazaar.com/wp-content/uploads/2018/12/internet-banking-1-.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://www.outlookindia.com/outlookmoney/public/uploads/article/gallery/f6eeec1300860ca890c3ace48c4548bd.jpg" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+</section>
+
+
+<section class="white-section" id="features">
+   <div class="container-fluid">
+
+     <div class="row">
+
+       <div class="feature-box col-lg-4">
+         <i class="icon fas fa-check-circle fa-4x"></i>
+         <h3 class="feature-title">Money Transfer now made more easy</h3>
+         <p>Send & receive money, anywhere & anytime</p>
+       </div>
+
+       <div class="feature-box col-lg-4">
+         <i class="icon fas fa-history fa-4x"></i>
+         <h3 class="feature-title">Check your transaction history anytime</h3>
+         <p>Keep record of your transaction details</p>
+       </div>
+
+       <div class="feature-box col-lg-4">
+         <i class="icon fas fa-users fa-4x"></i>
+         <h3 class="feature-title">Create Bank Accounts</h3>
+         <p>You can create as many accounts as much you want</p>
+       </div>
+     </div>
+
+   </div>
+ </section>
+
+
+
+<section id="contact">
+ <div class="last">
+         <div class="gh">
+   <h1 class="lets">Let's get connected!!!</h1>
+   <p class="gh">Contact us:</p>
+ </div>
+     
+     <a class="special" href="https://github.com/its-kaustubh"><i class="fab fa-github fa-1x cricket"></i> GitHub</a>
+   
+     <a class="special" href="mailto:madebykaustubhpatil@gmail.com"><i class="far fa-envelope fa-1x cricket"></i> Send a Mail</a>
+     
+
+
+
+ </div>
+ </section>
+<footer class="text-center mt-5 py-2">
+  <p>&copy 2021, BIG MONEY BANK <br> This website is made and maintained by <b>KAUSTUBH PATIL</b> </p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+  </body>
+</html>
---
+++ Basic Banking System/navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar -->
+      <nav class="navbar navbar-expand-md navbar-light bg-warning">
+      <a class="navbar-brand" href="index.php" style="color:#4B0082;"><b>BIG MONEY BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color: #ff0000;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color: #ff0000;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color: #ff0000;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color: #ff0000;"><b>Transaction History</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php" style="color: #ff0000;"><b>About Us</b></a>
+              </li>
+          </div>
+       </nav>
